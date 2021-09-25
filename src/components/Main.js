36bd6d304eb703d373aa17c0d603ca375b029b15
genta <yasokawag@gmmail.com>
2021-09-26 00:33:53 +0900

    #5

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React,{useState,useEffect} from 'react';
 import Search from "./Search";
 import Map from "./Map";
 import Video from "./Video";
 import Sns from "./Sns";
 import styled from "styled-components"
 
-class Main extends React.Component{
-    render(){
+function Main (){
+
+
+    const [Post,setPost] = useState([]);
+    const [Caption,setCaption] = useState([]);
+        useEffect(() => {
+            fetch("https://graph.facebook.com/17841564013123618/top_media?user_id=17841411766795081&fields=id,media_url,caption,permalink,media_type,comments_count,like_count&access_token=EAA9mxzmBApoBANQKYIlJnl0iLdewqw10sROg4d9IOWZAulfN0JkeMp6jMB4JH4pkYzUVJXuVyzRgBWmqlGCUaN3OyZBXq6ZAwyZBj4vFkoZBOeNUaVJH397z1uiRWVe8IOsrkeEGeQTMHMxzZBaG59peUnJiTKZCPgQzjXnbHSNJhj4h4PtjtqD")
+              .then((response) => response.json())
+              .then((response) => {
+              
+                console.log(response.data[0].media_url)
+                setPost(response.data[0].media_url);
+                setCaption(response.data[0].caption);
+              });
+               
+            //   .then((data) => {
+            //         const image = data.caption
+            //         const caption = data[0]
+            //       });
+            
+          }, 
+          
+         
+          []);
+
         return(
          <MainWrapper> 
             <TopWrapper>
@@ -16,13 +39,13 @@ class Main extends React.Component{
              <Map/>
             </MiddleWrapper>
             <BottomWrapper>
-             <Sns/> 
+             <Sns Post={Post} Caption={Caption}/> 
             </BottomWrapper>
             
          </MainWrapper>
         );
     }
-}
+
 
 export default Main;
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import styled from "styled-components"
+// import React from 'react';
+// import styled from "styled-components"
 
 
-class Searchbar extends React.Component {
-    handleChange = (event) => {
-        this.setState({
-            term: event.target.value
-        });
+// class Searchbar extends React.Component {
+//     handleChange = (event) => {
+//         this.setState({
+//             term: event.target.value
+//         });
     
-    };
-    handleSubmit = event => {
-        event.preventDefault();
-        this.props.handleFormSubmit(this.state.term);
-    }
+//     };
+//     handleSubmit = event => {
+//         event.preventDefault();
+//         this.props.handleFormSubmit(this.state.term);
+//     }
 
-    render() {
+//     render() {
         
-        return (
+//         return (
             
         
-            <TopSearchWrapper>
-                <form onSubmit={this.handleSubmit} className='ui form'>
-                    <div className='field'>
-                        <input onChange={this.handleChange} name='video-search' type="text" placeholder="Search.."/>
-                    </div>
-                </form>
-            </TopSearchWrapper>
+//             <TopSearchWrapper>
+//                 <form onSubmit={this.handleSubmit} className='ui form'>
+//                     <div className='field'>
+//                         <input onChange={this.handleChange} name='video-search' type="text" placeholder="Search.."/>
+//                     </div>
+//                 </form>
+//             </TopSearchWrapper>
                 
-        )
-    }
-}
-export default Searchbar;
+//         )
+//     }
+// }
+// export default Searchbar;
 
 
-const TopSearchWrapper = styled.div`
-top:10vh;
-position: relative;
+// const TopSearchWrapper = styled.div`
+// top:10vh;
+// position: relative;
 
-`
+// `
