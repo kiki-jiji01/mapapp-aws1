849b1a96137afef27682c7176373ba3bd8b700be
genta <yasokawag@gmmail.com>
2021-10-04 21:44:46 +0900

    modified css

--- src/App.js
+++ src/App.js
@@ -72,9 +72,7 @@ const App = () => {
                 <Banner>
                  <h2>Map With</h2>
                  <p>
-                 Where do you wanna live with your beatiful partner in the future?<br/>
-                 If you wish strong and strong, You can go everywhere you want.<br/> Let's start your dream here Get
-                 
+                 Where do you wanna live with your beatiful partner in the future?
                  </p>
 
 
@@ -127,22 +125,32 @@ export default App;
 const Topwrapper = styled.div`
 height: 150vh;
 position: relative;
-background: url("https://images.unsplash.com/photo-1446776653964-20c1d3a81b06?ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60")center center;
-
+background: url("https://images.unsplash.com/photo-1446776653964-20c1d3a81b06?ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60")center center ;
+width:100%;
 `
 
 const Banner= styled.div`
-background-color: black;
+
 color: white;
-padding-top: 25vh;
-padding-left: 50px;
-padding-right: 50px;
-padding-bottom: 40px;
-width:400px;
+position: absolute;
+margin: auto;
+width:100%;
+padding: 50px;
+top:  30vh;                    /* 上からの位置指定 */
+bottom:  70vh;                 /* 下からの位置指定 */
+left:  0;                   /* 左からの位置指定 */
+right:  0;  
+line-height:normal;
+text-align:center;
+
+h2{
+    font-weight: 700;
+    font-size: 30px;
+}
 
 p{
-    font-weight: 200;
-    font-size: 18px;
+    font-weight: 400;
+    font-size: 24px;
 }
 
 `
