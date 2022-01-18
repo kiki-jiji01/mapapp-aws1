27e71dfdd6572493b952e63e342da165e75ade65
genta <yasokawag@gmmail.com>
2022-01-19 01:44:28 +0900

    #65

--- src/countries/components/CountryDetail.js
+++ src/countries/components/CountryDetail.js
@@ -4,6 +4,10 @@ import { useParams } from "react-router"
 import { API } from "../api"
 import {useHistory, NavLink} from 'react-router-dom';
 import { AuthContext } from "../contexts/AuthContext"
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import styled from "styled-components"
 
 export function CountryDetail() {
     const [country, setCountry] = useState(null)
@@ -27,31 +31,113 @@ export function CountryDetail() {
     }, [id, token])
 
     return (
-        <div>
-            
-            {!country && "Loading..."}
-            {country && (
-                <div>
-                     {country.country_image && (
-                        <img src={country.country_image}  alt={country.country_image} />
-                      )}
-                    {country.country_name}
-                    {country.is_owner && (
-                    <div>
-                    <NavLink to={`/countries/${country.id}/update`}>
-                   Update
-                    </NavLink>
-                    <NavLink to={`/countries/${country.id}/delete`}>
-                   Delete
-                    </NavLink>
-                    </div>
-                    )}
-                </div>
-                
-            )}
+        
+      <Detail>
+      {country && (
+      <Grid container spacing={2}>
+        <Grid item xs={5}  style={{height: "80vh", paddingLeft: "16px", paddingRight:"16px"}}>
+         {country.country_image && (
+            <CountryImg>
+                <img   
+                src={country.country_image}  
+                alt={country.country_image} 
+                style={{objectFit:'cover', objectPosition: "center" , width: "100%" ,height: "100%"}}   
+                />
+            </CountryImg>  
+          )}
+        </Grid>
+        <Grid item xs={7}  style={{height: "80vh"}}>
+            <CountryContent>
+              <NameContent>
+                 <h1>{country.country_name}</h1>
+              </NameContent>
+              <Border></Border>
+              <ContentContent>
+                <p>{country.content}</p>
+              </ContentContent>
+            </CountryContent>
+        </Grid>
+      </Grid>
+      )}
+      </Detail>
+     
+     
+            //   {country.is_owner && (
+            //   <div>
+            //   <NavLink to={`/countries/${country.id}/update`}>
+            //  Update
+            //   </NavLink>
+            //   <NavLink to={`/countries/${country.id}/delete`}>
+            //  Delete
+            //   </NavLink>
+            //   </div>
+            //   )}
+         
+          
+      
            
-        </div>
+        
     )
 }
 
 export default CountryDetail
+
+
+const Detail = styled.div`
+
+margin-top: 20vh;
+height: 100vh;
+width: 100vw;
+`
+
+const Img = styled.div`
+
+
+`
+
+const CountryImg = styled.div`
+height: 100%;
+width: 100%;
+overflow: hidden;
+padding-right: 96px;
+padding-left: 64px;
+`
+
+const CountryImg1 = styled.div`
+
+`
+
+const CountryContent = styled.div`
+
+padding: 10% ;
+margin-right: 20%;
+display: flex;
+text-align: center;
+// align-items: center;
+flex-direction: column;
+`
+
+const NameContent = styled.div`
+
+padding: 2% 10% ;
+
+`
+
+const ContentContent = styled.div`
+
+
+`
+const Border = styled.div`
+align-self: end;
+height: 20px;
+background-color: rgba(192,192,192,0.8);
+font-size: 0;
+line-height: 0;
+width: 40vw;
+margin-bottom: 15vh;
+margin-right: -50%;
+align-items: flex-end;
+`
+
+
+
